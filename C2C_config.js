56343277368b390568c89098446eb2d7c42bb8c1
Jamford <92556115+northmap@users.noreply.github.com>
2021-10-24 09:28:37 +0100

    Update C2C_config.js

--- C2C_config.js
+++ C2C_config.js
@@ -1,9 +1,9 @@
 
 const config = {
-    style: "mapbox://styles/jbamford/ckg0oa5iv14es19o58hvjucg2",
+    style: "mapbox://styles/jbamford/ckuiw9h5p5iew18k6sjgvqkdl",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
     CSV: "https://docs.google.com/spreadsheets/d/1fq76srdzD0AR4pULNNpx_Mja0zq0vv8yfOZd39Q7RNs/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-7.734, 54.262], //Lng, Lat
+    center: [-7.71333,54.27536], //Lng, Lat
     zoom: 11, //Default zoom
     pitch: 50,
    
