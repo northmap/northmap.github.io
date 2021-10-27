1660e2e8a90082f9ccc97d991e7fc8a577fb90dd
Jamford <92556115+northmap@users.noreply.github.com>
2021-10-27 17:52:45 +0100

    Update C2C_config.js

--- C2C_config.js
+++ C2C_config.js
@@ -3,8 +3,8 @@ const config = {
     style: "mapbox://styles/jbamford/ckuiw9h5p5iew18k6sjgvqkdl",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
     CSV: "https://docs.google.com/spreadsheets/d/1yg0yXP1qXZWMeFvFmxhHLmWlqRZ9FX4Likhe6zznkDo/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-7.664,54.197], //Lng, Lat
-    zoom: 9.57, //Default zoom
+    center: [-7.693,4.223], //Lng, Lat
+    zoom: 9.34, //Default zoom
     pitch: 10,
    
     title: "",
