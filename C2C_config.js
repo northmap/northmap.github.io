4dc758f1dac2f9324b09abdb358d8d8f92c4d5c7
Jamford <92556115+northmap@users.noreply.github.com>
2021-10-27 18:06:24 +0100

    Update C2C_config.js

--- C2C_config.js
+++ C2C_config.js
@@ -3,11 +3,9 @@ const config = {
     style: "mapbox://styles/jbamford/ckuiw9h5p5iew18k6sjgvqkdl",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
     CSV: "https://docs.google.com/spreadsheets/d/1yg0yXP1qXZWMeFvFmxhHLmWlqRZ9FX4Likhe6zznkDo/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-7.647005, 54.186177], //Lng, Lat
-    zoom: 9.34, //Default zoom
-    pitch: 32.50,
-    bearing: 19.20,
-
+    center: [-7.664,54.197], //Lng, Lat
+    zoom: 9.57, //Default zoom
+    pitch: 10,
    
     title: "",
     sideBarInfo: ["Title English", "Poet"],
