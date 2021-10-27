57fd0d926965dced224b0361761e3789d3f2c13c
Jamford <92556115+northmap@users.noreply.github.com>
2021-10-27 17:59:20 +0100

    Update C2C_config.js

--- C2C_config.js
+++ C2C_config.js
@@ -5,7 +5,9 @@ const config = {
     CSV: "https://docs.google.com/spreadsheets/d/1yg0yXP1qXZWMeFvFmxhHLmWlqRZ9FX4Likhe6zznkDo/gviz/tq?tqx=out:csv&sheet=Sheet1",
     center: [-7.693,4.223], //Lng, Lat
     zoom: 9.34, //Default zoom
-    pitch: 10,
+    pitch: 31.50,
+    bearing: 24.00,
+
    
     title: "",
     sideBarInfo: ["Title English", "Poet"],
