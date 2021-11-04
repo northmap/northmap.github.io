6c8e6b48a4b75732f605072a7519f3dfae6f3030
Jamford <92556115+northmap@users.noreply.github.com>
2021-11-04 23:51:48 +0000

    Update C2C_app.js

--- C2C_app.js
+++ C2C_app.js
@@ -478,7 +478,7 @@ map.on("load", function () {
                         16,
                         5
                       ],
-                    "circle-opacity": 1
+                    "circle-opacity": 0.1
                 }
 
             });
