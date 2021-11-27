8818272cb2b67692a699adb0b07dff58e9b65ccf
northmap <northmappingservices@gmail.com>
2021-11-27 23:43:06 +0000

    Update C2C_app.js

--- C2C_app.js
+++ C2C_app.js
@@ -27,7 +27,7 @@ function flyToLocation(currentFeature, zoom) {
     // console.log(modified_feature);  
     map.flyTo({
         center: currentFeature,
-        offset: [0, -200],
+        offset: [0, -100],
         zoom: 13,
         pitch: 0,
         speed: 0.3, // make the flying slow
