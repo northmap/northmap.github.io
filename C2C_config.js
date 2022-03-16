90ccb7711614ac542f01ee88ecd97451772a1403
northmap <northmappingservices@gmail.com>
2022-03-16 14:12:00 +0000

    Update C2C_config.js

--- C2C_config.js
+++ C2C_config.js
@@ -3,10 +3,10 @@ const config = {
     style: "mapbox://styles/jbamford/ckuiw9h5p5iew18k6sjgvqkdl",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
     CSV: "https://docs.google.com/spreadsheets/d/1yg0yXP1qXZWMeFvFmxhHLmWlqRZ9FX4Likhe6zznkDo/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-7.647005, 54.186177], //Lng, Lat
-    zoom: 9.02, //Default zoom
-    bearing: 19.20,
-    pitch: 32.50,
+    center: [-7.644402, 54.246548], //Lng, Lat
+    zoom: 9.43, //Default zoom
+    bearing: 22.32,
+    pitch: 33.37,
    
     title: "",
     sideBarInfo: ["Title English", "Poet"],
