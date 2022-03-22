f92738911d93acf5956fa89a9605f88c5aa3f3a6
northmap <northmappingservices@gmail.com>
2022-03-22 10:57:47 +0000

    Update Cuilcagh_config.js

--- Cuilcagh_config.js
+++ Cuilcagh_config.js
@@ -2,7 +2,7 @@
 const config = {
     style: "mapbox://styles/jbamford/ckuiw9h5p5iew18k6sjgvqkdl",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
-    CSV: "Poems_table.csv",
+    CSV: "https://docs.google.com/spreadsheets/d/1yg0yXP1qXZWMeFvFmxhHLmWlqRZ9FX4Likhe6zznkDo/gviz/tq?tqx=out:csv&sheet=Sheet1",
     center: [-7.644402, 54.246548], //Lng, Lat
     zoom: 9.43, //Default zoom
     bearing: 22.30,
