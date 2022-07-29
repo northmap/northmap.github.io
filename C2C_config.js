a3af3716036165dbb45211cc6ee0fd86d68fdf9e
northmap <northmappingservices@gmail.com>
2022-07-29 15:53:35 +0100

    changes

--- C2C_config.js
+++ C2C_config.js
@@ -1,7 +1,7 @@
 const config = {
     style: "mapbox://styles/jbamford/ckg0oa5iv14es19o58hvjucg2",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
-    CSV: 'https://27744145-ae97-4724-afaa-408169578b05.usrfiles.com/ugd/277441_1191af4155594447af687fe4e80df900.csv', 
+    CSV: "https://docs.google.com/spreadsheets/d/1fq76srdzD0AR4pULNNpx_Mja0zq0vv8yfOZd39Q7RNs/gviz/tq?tqx=out:csv&sheet=Sheet1", 
     center: [-7.734, 54.262], //Lng, Lat
     zoom: 11, //Default zoom
     pitch: 50,
