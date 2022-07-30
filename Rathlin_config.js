ee99842be90458bade402a1d03d9928dbc706935
northmap <northmappingservices@gmail.com>
2022-07-30 12:57:20 +0100

    Update Rathlin_config.js

--- Rathlin_config.js
+++ Rathlin_config.js
@@ -2,13 +2,13 @@
 const config = {
     style: "mapbox://styles/jbamford/cl4idnsgn001g14qqqnnjv455",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoiY2w2Nm8yZnNoMDV3ODNjdGRpMjVmMnFiMSJ9.UcdpyCBAekPVqaturRx5Xg",
-    CSV: "https://docs.google.com/spreadsheets/d/1fq76srdzD0AR4pULNNpx_Mja0zq0vv8yfOZd39Q7RNs/gviz/tq?tqx=out:csv&sheet=Sheet1",
+    CSV: "https://docs.google.com/spreadsheets/d/13sSrHjS95ZpqPo5OhdxYE87ekgSNpum28MejC-lZMSU/gviz/tq?tqx=out:csv&sheet=Sheet1",
     center: [-6.214, 55.286], //Lng, Lat
     zoom: 12.50, //Default zoom
     pitch: 5,
    
     title: "",
-    sideBarInfo: ["Subject", "Type"],
+    sideBarInfo: ["Title/name", "Theme"],
     popupInfo: ["Description",],
     filters: [
         
